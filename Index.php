9904179b5e28b05ad233ac2686f02bfea7a540af
f <f>
2017-11-29 17:07:20 +0700

    Расчет высоты треугольника по трем сторонам

--- Index.php
+++ Index.php
@@ -19,7 +19,7 @@
 		?>
 		<img src="area_triangle.png" alt="Обозначение входных величин">		
 		<p>Рассчет высоты <b>h</b> к стороне <b>а</b>: </p>
-		<form action="Index.php" method="GET">
+		<form action="index.php" method="GET">
 			Сторона a:
 			<input type="text" name="value1"
 				value="<?= htmlspecialchars($value1)?>">
@@ -34,21 +34,24 @@
 				<br><br>
 			<input type="submit" name="operation" value="Рассчитать высоту">			
 		</form>
-		<?php			
-			// Проверка, чтобы были введены все три значения, причем числовые и больше нуля
-			if (isset($_GET['operation']) && isset($_GET['value1']) && is_numeric($_GET['value1']) && ($_GET['value1'])>0 && isset($_GET['value2']) && is_numeric($_GET['value2']) && ($_GET['value2'])>0 && isset($_GET['value3']) && is_numeric($_GET['value3']) && ($_GET['value3'])>0){	
-				//проверка на существавание треугольника (сумма двух любых сторон должна быть больше третьей)
-				if ((($_GET['value1'])+($_GET['value2']))>($_GET['value3']) && (($_GET['value2'])+($_GET['value3']))>($_GET['value1']) && (($_GET['value1'])+($_GET['value3']))>($_GET['value2'])){	
-					$P = 1/2*($_GET['value1']+$_GET['value2']+$_GET['value3']);
-					$H = number_format((2*sqrt($P*($P-$_GET['value1'])*($P-$_GET['value2'])*($P-$_GET['value3'])))/$_GET['value1'],2, ',','');
-					echo "Высота треугольника: $H";
-				}else {
-				echo "Треугольника с такими сторонами не существует";
-				}			
-			}
-			else {
-				echo "Проверьте корректность введенных данных!";
-			};				
+		<?php				
+			// Проверка нажатия кнопки
+			if (isset($_GET['operation'])) {
+				// Проверка, чтобы были введены все три значения, причем числовые и больше нуля
+				if (isset($_GET['value1']) && is_numeric($_GET['value1']) && ($_GET['value1'])>0 && isset($_GET['value2']) && is_numeric($_GET['value2']) && ($_GET['value2'])>0 && isset($_GET['value3']) && is_numeric($_GET['value3']) && ($_GET['value3'])>0){	
+					//проверка на существавание треугольника (сумма двух любых сторон должна быть больше третьей)
+					if ((($_GET['value1'])+($_GET['value2']))>($_GET['value3']) && (($_GET['value2'])+($_GET['value3']))>($_GET['value1']) && (($_GET['value1'])+($_GET['value3']))>($_GET['value2'])){	
+						$P = 1/2*($_GET['value1']+$_GET['value2']+$_GET['value3']);
+						$H = number_format((2*sqrt($P*($P-$_GET['value1'])*($P-$_GET['value2'])*($P-$_GET['value3'])))/$_GET['value1'],2, ',','');
+						echo "Высота треугольника: $H";
+					}else {
+					echo "Треугольника с такими сторонами не существует";
+					}			
+				}
+				else {
+					echo "Проверьте корректность введенных данных!";
+				}
+			}			
 		?>
 	</body>
 </html>
